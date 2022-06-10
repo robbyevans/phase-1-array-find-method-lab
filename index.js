3a019919bb807ebe621d628b3731be20d4114a2e
Evans Rutto <robbyevans001@gmail.com>
2022-06-10 10:07:17 +0300

    commit tree 7bf99ddbf53cd46dbfd195141057abe71001c742

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ];
+
+// for (items of record){
+//   console.log (items['year'])
+// }
+
+
+function superbowlWin(record){
+    for (let items of record){
+        if(items.result==='W'){
+            return(items.year)
+        }
+}
+}
+
+let findItem = record.find(superbowlWin);
+
+
+//   for (items of record){
+//     for(keys in items){
+//     if (keys.result === "W"){
+//       return keys['year'];
+//     }
+//   else{
+//     return undefined;
+//   }
+//   }
+//   }
+// }
+
+
+// console.log (findItem);
